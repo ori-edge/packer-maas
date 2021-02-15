9af0403d359e8faad50c4a39d8982f7754b6af34
Adam Furminger <adam@ori.co>
2021-02-15 14:30:54 +0000

    fixing quotes + modules in build.sh

--- vmware-esxi/build.sh
+++ vmware-esxi/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 IMAGE=VMware-ESXi-7.0.1-16850804-HPE-701.0.0.10.6.0.40-Oct2020.iso
 
-sudo PACKER_LOG=1 packer build -var 'vmware_esxi_iso_path=/home/adam/$IMAGE' vmware-esxi.json
+sudo modprobe nbd
+sudo PACKER_LOG=1 packer build -var "vmware_esxi_iso_path=/home/adam/$IMAGE" vmware-esxi.json
